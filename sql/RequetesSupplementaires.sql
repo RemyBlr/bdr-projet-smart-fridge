08deda1396d5d6c13749c803b7bfdc5118516eb6
CristhianRo <114008453+CristhianRo@users.noreply.github.com>
2024-01-21 16:54:23 +0100

    4 requêtes supplémentaires

--- sql/RequetesSupplementaires.sql
+++ sql/RequetesSupplementaires.sql
@@ -22,8 +22,6 @@ SELECT  i_nom, i_type
 FROM Ingredients_recette
 WHERE r_nom = 'Sauce bolognese maison';
 
-select *
-from Ingredients_recette;
 
 -- Requête 2 : Trouver pour un ingrédient principal, les ingrédients sustituables
 DROP VIEW IF EXISTS Ingredients_sustituables;
@@ -41,5 +39,45 @@ CREATE OR REPLACE VIEW Ingredients_sustituables AS
     INNER JOIN ingredient i
         ON isu.ingredient_substitue = i.id;
 
-SELECT *
+SELECT distinct *
 FROM Ingredients_sustituables;
+
+-- Requête 3 : Trouver les ingrédients d'une recette que l'utilisateur aime
+DROP VIEW IF EXISTS Ingredients_aime;
+
+CREATE OR REPLACE VIEW Ingredients_aime AS
+    SELECT  u.prenom,
+            u.nom,
+            u.email,
+            ir.r_nom,
+            ir.i_nom ingredient_aime_nom,
+            ir.i_type ingredient_aime_type
+
+    FROM ingredients_recette ir
+    INNER JOIN aime_ingredient ai
+        ON ir.i_id = ai.ingredient
+    INNER JOIN utilisateur u
+        ON u.email = ai.utilisateur;
+
+SELECT *
+FROM Ingredients_aime;
+
+-- Requête 4 : Trouver les ingrédients d'une recette pour lesquels l'utilisateur a une allergie
+DROP VIEW IF EXISTS Ingredients_allergie;
+
+CREATE OR REPLACE VIEW Ingredients_allergie AS
+    SELECT  u.prenom,
+            u.nom,
+            u.email,
+            ir.r_nom,
+            ir.i_nom ingredient_allergie_nom,
+            ir.i_type ingredient_allergie_type
+
+    FROM ingredients_recette ir
+    INNER JOIN allergie al
+        ON ir.i_id = al.ingredient
+    INNER JOIN utilisateur u
+        ON u.email = al.utilisateur;
+
+SELECT *
+FROM Ingredients_allergie;
